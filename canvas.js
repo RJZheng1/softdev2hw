a6f12a9ef9c8f71d28c332f0bfde5b78b1b8f8c7
RJZheng1 <stuykid2016@yahoo.com>
2016-02-11 00:45:05 -0500

    Added stuff.

--- canvas.js
+++ canvas.js
@@ -1,32 +1,67 @@
 var c = document.getElementById("ftb2maga");
-var con = c.getContext("2d");
+var ctx = c.getContext("2d");
 
-/*
-con.fillStyle = "#ABCDEF";
-con.fillRect(0, 0, 300, 300);
+var x = 150;
+var y = 150;
+var r = 100;
 
-con.strokeStyle = "#ADD8E6"
-con.strokeRect(100, 100, 400, 400);
+ctx.fillStyle = "#CC0000";
+ctx.strokeStyle = "#CC0000";
 
-con.beginPath();
-con.moveTo(100, 400);
-con.lineTo(100, 100);
-con.moveTo(0,0);
-con.stroke();
-con.lineTo(350, 325);
-con.lineTo(100, 400);
-con.fill();
-con.closePath();
-*/
-
-var x = 300
-var y = 300;
-var r = 50;
-
-con.beginPath();
-con.moveTo(x, y + r);
-for(var theta = 0; theta < 2 * Math.PI; theta += Math.PI / 3){
-    con.lineTo(x + r*Math.cos(theta), y + r*Math.sin(theta));
+for(var theta = 0; theta <= 2 * Math.PI; theta += Math.PI / 12){
+    //Starts a path. Erases old path.
+    ctx.beginPath();
+    /*
+      Takes two parameters: x-coordinate, y-coordinate
+      Moves the path to a certain coordinate without drawing a line
+    */
+    ctx.moveTo(x + r * Math.cos(theta), y + r * Math.sin(theta));
+    for(var angle = theta; angle <= 2 * Math.PI + theta; angle += Math.PI / 3){
+	/*
+	  Takes two parameters: x-coordinate, y-coordinate
+	  Moves the path to a certain point by drawing a line between the
+	  current point and the new point
+	*/
+	ctx.lineTo(x + r*Math.cos(angle), y + r*Math.sin(angle));
+    }
+    //Draws a line from the current point to the starting point
+    ctx.closePath();
+    //Draws the path that you have created
+    ctx.stroke();
 }
-con.closePath();
-con.fill();
+
+ctx.strokeStyle = "#000000";
+
+ctx.beginPath();
+/*
+  Takes five parameters: x-coordinate, y-coordinate, radius, start angle, end angle
+  The x and y coordinates represent the center of the arc
+  It will add an arc with the specified parameters to your path
+*/
+ctx.arc(x, y, r, 0, 2*Math.PI);
+ctx.stroke();
+
+r *= Math.sqrt(7/9);
+
+ctx.beginPath();
+ctx.arc(x, y, r, 0, 2*Math.PI);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.moveTo(x, y - r);
+ctx.lineTo(x + r*Math.sqrt(3)/2, y - r/2);
+ctx.lineTo(x + r*Math.sqrt(3)/2, y + r/2);
+ctx.lineTo(x, y + r);
+ctx.lineTo(x - r*Math.sqrt(3)/2, y + r/2);
+ctx.lineTo(x - r*Math.sqrt(3)/2, y - r/2);
+ctx.closePath();
+//Fills in the shape enclosed by the path you created
+ctx.fill();
+
+ctx.fillStyle = "#000000"
+ctx.font = "30px arial";
+/*
+  Takes 3 parameters: text, x-coordinate, y-coordinate
+  Displays the text at the given coordinates
+*/
+ctx.fillText("Hello World!", 0, 30);
