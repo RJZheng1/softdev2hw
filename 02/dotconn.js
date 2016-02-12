bf2d70e903a71a9c32b3620b66995c2ffe5fbef6
RJZheng1 <stuykid2016@yahoo.com>
2016-02-12 09:54:41 -0500

    offsetX and offsetY are useful.

--- 02/dotconn.js
+++ 02/dotconn.js
@@ -3,31 +3,24 @@ var ctx = canvas.getContext("2d");
 ctx.strokeStyle = "#000000";
 ctx.fillStyle = "#000000";
 
-var x = -1;
-var y = -1;
 var r = 10;
-var rect = canvas.getBoundingClientRect();
-
-ctx.strokeRect(0, 0, canvas.width, canvas.height);
 
 var connect = function connect(e){
+    e.preventDefault();
+
+    ctx.lineTo(e.offsetX, e.offsetY);
+    ctx.stroke();
+
     ctx.beginPath();
-    ctx.arc(e.pageX - rect.left, e.pageY - rect.top, r, 0, 2*Math.PI);
+    ctx.arc(e.offsetX, e.offsetY, r, 0, 2*Math.PI);
     ctx.fill();
-    if(x != -1){
-	ctx.beginPath();
-	ctx.moveTo(x, y);
-	ctx.lineTo(e.pageX - rect.left, e.pageY - rect.top);
-	ctx.stroke();
-    }
-    x = e.pageX - rect.left;
-    y = e.pageY - rect.top;
+
+    ctx.beginPath();
+    ctx.moveTo(e.offsetX, e.offsetY);
 };
 
 var clear = function clear(e){
     e.preventDefault();
-    x = -1;
-    y = -1;
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.strokeRect(0, 0, canvas.width, canvas.height);
 };
