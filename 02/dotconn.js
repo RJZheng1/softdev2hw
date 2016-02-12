5500bd08e5c870a2eebd943939ab0f30414b5bfe
RJZheng1 <stuykid2016@yahoo.com>
2016-02-11 21:40:42 -0500

    Connect the dots.

--- 02/dotconn.js
+++ 02/dotconn.js
@@ -1,31 +1,35 @@
-var ctx = document.getElementById("playground").getContext("2d");
-
+var canvas = document.getElementById("playground")
+var ctx = canvas.getContext("2d");
 ctx.strokeStyle = "#000000";
 ctx.fillStyle = "#000000";
 
 var x = -1;
 var y = -1;
 var r = 10;
+var rect = canvas.getBoundingClientRect();
+
+ctx.strokeRect(0, 0, canvas.width, canvas.height);
 
 var connect = function connect(e){
     ctx.beginPath();
-    ctx.arc(e.clientX, e.clientY, r, 0, 2*Math.PI);
+    ctx.arc(e.pageX - rect.left, e.pageY - rect.top, r, 0, 2*Math.PI);
     ctx.fill();
     if(x != -1){
 	ctx.beginPath();
 	ctx.moveTo(x, y);
-	ctx.lineTo(e.clientX, e.clientY);
+	ctx.lineTo(e.pageX - rect.left, e.pageY - rect.top);
 	ctx.stroke();
     }
-    x = e.clientX;
-    y = e.clientY;
+    x = e.pageX - rect.left;
+    y = e.pageY - rect.top;
 };
 
 var clear = function clear(e){
     e.preventDefault();
     x = -1;
     y = -1;
-    ctx.clearRect(0, 0, 400, 400);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.strokeRect(0, 0, canvas.width, canvas.height);
 };
 
 document.getElementById("playground").addEventListener("click", connect);
