7afaa40366e309ffb21f38a8afc39b22bfca267d
RJZheng1 <stuykid2016@yahoo.com>
2016-04-12 23:11:29 -0400

    Decorator + Flask

---
+++ hw10/app.py
@@ -0,0 +1,64 @@
+from flask import Flask, render_template, request, jsonify
+from functools import wraps
+import random, json, re
+from math import radians, cos, sin, asin, sqrt
+import time
+
+app = Flask(__name__)
+global cities
+
+def printArgs(funct):
+    def inner(f):
+        @wraps(f)
+        def wrapped(*args):
+            print f.func_name + str(args)
+            return f(*args)
+        return wrapped
+    return inner
+
+def timeFunc(funct):
+    def inner(f):
+        @wraps(f)
+        def wrapped(*args):
+            t = time.time()
+            output = f(*args)
+            print "Executed in " + str(time.time() - t) + " seconds"
+            return output
+        return wrapped
+    return inner
+
+@app.route("/")
+@timeFunc("index")
+@printArgs("index")
+def index():
+    return render_template("index.html");
+
+@app.route("/distance")
+@timeFunc("index")
+@printArgs("index")
+def distance():
+    lat1 = radians(float(request.args.get("lat1").encode('ascii', 'ignore')))
+    lon1 = radians(float(request.args.get("lon1").encode('ascii', 'ignore')))
+    lat2 = radians(float(request.args.get("lat2").encode('ascii', 'ignore')))
+    lon2 = radians(float(request.args.get("lon2").encode('ascii', 'ignore')))
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
+    a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
+    c = 2 * asin(sqrt(a))
+    r = 6371
+    return str(c * r)
+
+@app.route("/getCoordinates")
+@timeFunc
+@printArgs
+def getPlace():
+    coords = cities[random.randint(0,len(cities)-1)].split('\t')
+    c = {'lat':coords[0],'lng':coords[1]}
+    return jsonify(c)
+
+if __name__ == "__main__":
+    f = open('cities15000.txt', 'r')
+    cities = f.read().split('\n')
+    f.close()
+    app.debug = True
+    app.run(host = "0.0.0.0", port=8000)
